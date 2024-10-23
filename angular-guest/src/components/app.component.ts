c92e6cdef161c7d8843ab3f0716ed3b79446acc8
Naif Alsultan <contact@naifalsultan.com>
2024-10-23 21:19:49 +0300

    add cross-app communication

--- angular-guest/src/components/app.component.ts
+++ angular-guest/src/components/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 
 @Component({
   selector: "root",
@@ -6,9 +6,17 @@ import { Component } from "@angular/core";
   template: `<div style="border: 1px dashed red; margin: 16px; padding: 16px">
     <p>Angular Guest</p>
     <p>Value: {{ value }}</p>
-    <button (click)="value = value + 1">Increment</button>
+    <button (click)="increment()">Increment</button>
   </div> `,
 })
 export class AppComponent {
   value = 0;
+  increment() {
+    const event = new CustomEvent("increment", { detail: ++this.value });
+    window.dispatchEvent(event);
+  }
+  @HostListener("window:increment", ["$event.detail"])
+  update(newValue: number) {
+    this.value = newValue;
+  }
 }
--- angular-host/src/components/home.component.ts
+++ angular-host/src/components/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { MicrofrontendComponent } from "./microfrontend.component";
 
@@ -8,11 +8,19 @@ import { MicrofrontendComponent } from "./microfrontend.component";
   template: `<div style="border: 1px dashed black; margin: 16px; padding: 16px">
     <p>Angular Host</p>
     <p>Value: {{ value }}</p>
-    <button (click)="value = value + 1">Increment</button>
+    <button (click)="increment()">Increment</button>
     <p><a routerLink="/other">Navigate Forward</a></p>
     <microfrontend remoteName="angular-guest" exposedModule="./mfe" />
   </div>`,
 })
 export class HomeComponent {
   value = 0;
+  increment() {
+    const event = new CustomEvent("increment", { detail: ++this.value });
+    window.dispatchEvent(event);
+  }
+  @HostListener("window:increment", ["$event.detail"])
+  update(newValue: number) {
+    this.value = newValue;
+  }
 }
